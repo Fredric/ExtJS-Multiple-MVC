4bee4f3939cf8bfc9da9e15db7840b514250549f
Fredric Berling <fredric.berling@gmail.com>
2012-11-25 23:09:10 +0100

    Fix for Ext 4.1.3

--- Portal/MVCHelper.js
+++ Portal/MVCHelper.js
@@ -1,7 +1,17 @@
 Ext.Class.registerPreprocessor('mvchelper',
     function (cls, data) {
         if (Ext.isArray(data.controllers) && !Ext.isArray(cls.controllers)) {
-            cls.controllers = data.controllers;
+
+           if(data.extend !== 'Ext.app.Application'){
+
+               if(cls.superclass && cls.superclass.$className === "Ext.app.Application"){
+                   //skip if its a Ext.Application
+               }else{
+                   cls.controllers = data.controllers;
+               }
+
+
+           }
         }
     }, true);
 Ext.Class.setDefaultPreprocessorPosition('mvchelper', 'first');
